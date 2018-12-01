efbdb5280b712e107f82084558a18c8b718e3f54
Andrey Tarantsov <andrey@tarantsov.com>
2018-12-01 22:00:30 +0300

    Create Cocoa app skeleton

---
+++ LiveReload/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LiveReload
+//
+//  Created by Andrey Tarantsov on 2018-12-01.
+//  Copyright © 2018 Andrey Tarantsov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LiveReload/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LiveReload
+//
+//  Created by Andrey Tarantsov on 2018-12-01.
+//  Copyright © 2018 Andrey Tarantsov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
